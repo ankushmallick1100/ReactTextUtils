1f548373b11e02af0f1144eba1e4e134d97a52af
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-01-24 16:25:56 +0530

    Changes Updated - 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
 
+let name = "<b>Ankush</b>"
 function App() {
-  return (<>
-    <div className="blank">Lovely</div>
-  </>
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello, {name}</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+          provident! Magni quo assumenda eius minima vero, doloribus id nemo
+          deleniti! Possimus rerum consectetur porro qui dicta, omnis eligendi a
+          dolor?
+        </p>
+      </div>
+    </>
   );
 }
 
